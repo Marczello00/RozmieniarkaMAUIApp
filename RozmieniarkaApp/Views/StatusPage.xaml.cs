f05d30e805805319682a8aad5fef454b03b6b111
Marczello00 <56977890+Marczello00@users.noreply.github.com>
2023-11-15 01:58:13 +0000

    Created animated gradient while refreshing

--- RozmieniarkaApp/Views/StatusPage.xaml.cs
+++ RozmieniarkaApp/Views/StatusPage.xaml.cs
@@ -7,7 +7,43 @@ public partial class StatusPage : ContentPage
     public StatusPage()
 	{
 		InitializeComponent();
-        BindingContext = new StatusPageViewModel();
+        Task.Run(AnimateMask);
+    }
+        private async void AnimateMask()
+    {
+        Action<double> tealMovement = tInput => {
+            firstStop.Offset = (float)tInput;
+            firstStop2.Offset = (float)tInput;
+            firstStop3.Offset = (float)tInput;
+            firstStop4.Offset = (float)tInput;
+            firstStop5.Offset = (float)tInput;
+            firstStop6.Offset = (float)tInput;
+            firstStop7.Offset = (float)tInput;
+            firstStop8.Offset = (float)tInput;
+        };
+        Action<double> orangeMovement = oInput => {
+            secondStop.Offset = (float)oInput;
+            secondStop2.Offset = (float)oInput;
+            secondStop3.Offset = (float)oInput;
+            secondStop4.Offset = (float)oInput;
+            secondStop5.Offset = (float)oInput;
+            secondStop6.Offset = (float)oInput;
+            secondStop7.Offset = (float)oInput;
+            secondStop8.Offset = (float)oInput;
+        };
+
+        while (true)
+        {
+            StatusItemsGrid.Animate(name: "forward", callback: tealMovement, start: 0, end: 1, length: 1000, easing: Easing.SinIn);
+            await Task.Delay(1000);
+            StatusItemsGrid.Animate(name: "backward", callback: tealMovement, start: 1, end: 0, length: 1000, easing: Easing.SinIn);
+            await Task.Delay(1000);
+
+            StatusItemsGrid.Animate(name: "forward2", callback: orangeMovement, start: 1, end: 0, length: 1000, easing: Easing.SinIn);
+            await Task.Delay(1000);
+            StatusItemsGrid.Animate(name: "backward2", callback: orangeMovement, start: 0, end: 1, length: 1000, easing: Easing.SinIn);
+            await Task.Delay(1000);
+        }
     }
 
 }
