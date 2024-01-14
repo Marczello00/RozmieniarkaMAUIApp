e57572c646325ffb0b2228837ac7e728f52dd90b
Marczello00 <56977890+Marczello00@users.noreply.github.com>
2024-01-14 19:13:57 +0000

    Started ver1.2 with leave confirmation popup

--- RozmieniarkaApp/AppShell.xaml.cs
+++ RozmieniarkaApp/AppShell.xaml.cs
@@ -6,5 +6,18 @@
         {
             InitializeComponent();
         }
+        protected override bool OnBackButtonPressed()
+        {
+            MakeSureUserWantsToExit();
+            return true;
+        }
+        private async void MakeSureUserWantsToExit()
+        {
+            bool answer = await DisplayAlert("Wyjście", "Czy na pewno chcesz wyjść?", "Tak", "Nie");
+            if (answer)
+            {
+                Application.Current.Quit();
+            }
+        }
     }
 }
